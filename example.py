8ec03767136cbbb844f553d4d1517089fc5d7bcc
Aldo Nicolás Mellado Opazo <aldomellado.1310@gmail.com>
2023-11-15 16:50:21 -0300

    up_to_date

---
+++ example.py
@@ -0,0 +1,9 @@
+import os
+
+# Docstring del módulo: Explica el propósito de este módulo
+"""
+Este módulo demuestra la importación y el uso del módulo 'os' en Python.
+"""
+
+# Usando 'os' para obtener el directorio de trabajo actual
+print(f"My current Working directory is: {os.getcwd()}")
